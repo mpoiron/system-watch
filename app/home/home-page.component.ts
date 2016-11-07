65c294bca9f7eaf2e6cbee6f403c9916ee16bc9f
Maxime Poiron <maxime.rp@outlook.fr>
2016-11-07 22:25:46 +0100

    More page display and process styles

--- app/home/home-page.component.ts
+++ app/home/home-page.component.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core'
 @Component({
     selector: 'home-page',
     templateUrl: './home-page.template.html',
-    styleUrls: ['./home-page.style.scss']
+    styleUrls: ['./home-page.style.scss'],
 })
 export class HomePageComponent {
 
